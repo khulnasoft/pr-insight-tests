9c0b5ceeb0401e0cb735f16d95dddea4aa2a7d30
gitworkflows <118260833+gitworkflows@users.noreply.github.com>
2024-07-29 07:36:08 +0600

    Update test_convert_to_markdown.py
    Signed-off-by: gitworkflows <118260833+gitworkflows@users.noreply.github.com>

--- tests/unittest/test_convert_to_markdown.py
+++ tests/unittest/test_convert_to_markdown.py
@@ -79,11 +79,9 @@ class TestBR:
         file_change_description = '- Imported `FilePatchInfo` and `EDIT_TYPE` from `pr_assistant.algo.types` instead of `pr_assistant.git_providers.git_provider`.'
         file_change_description_br = insert_br_after_x_chars(file_change_description)
         expected_output = ('<li>Imported <code>FilePatchInfo</code> and <code>EDIT_TYPE</code> from '
-                           '<code>pr_assistant.algo.types</code> instead <br>of '
+                           '<code>pr_assistant.algo.types</code> <br>instead of '
                            '<code>pr_assistant.git_providers.git_provider</code>.')
         assert file_change_description_br == expected_output
-        # print("-----")
-        # print(file_change_description_br)
 
     def test_br2(self):
         file_change_description = (
@@ -94,14 +92,11 @@ class TestBR:
                            'ColorPaletteResourcesCollection ColorPaletteResourcesCollection '
                            '</code><br><code>ColorPaletteResourcesCollection</code>')
         assert file_change_description_br == expected_output
-        # print("-----")
-        # print(file_change_description_br)
 
     def test_br3(self):
         file_change_description = 'Created a new class `ColorPaletteResourcesCollection` which extends `AvaloniaDictionary<ThemeVariant, ColorPaletteResources>` and implements aaa'
         file_change_description_br = insert_br_after_x_chars(file_change_description)
-        assert file_change_description_br == ('Created a new class <code>ColorPaletteResourcesCollection</code> which '
-                                              'extends <br><code>AvaloniaDictionary<ThemeVariant, ColorPaletteResources>'
-                                              '</code> and implements <br>aaa')
-        # print("-----")
-        # print(file_change_description_br)
+        expected_output = ('Created a new class <code>ColorPaletteResourcesCollection</code> which '
+                           'extends <br><code>AvaloniaDictionary<ThemeVariant, ColorPaletteResources>'
+                           '</code> and implements <br>aaa')
+        assert file_change_description_br == expected_output
